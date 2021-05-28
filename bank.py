c16ab85779b3bd7129fb80ec5c219292900fa905
fadallalaaisha <79199476+fadallalaaisha@users.noreply.github.com>
2021-05-28 20:00:53 +0300

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,10 @@
+class Bank:
+    place="Nairobi"
+
+def __init__(self,Id,Nationality,age):
+    self.Id=Id
+    self.Nationality=Nationality
+    self.age=age
+
+def behavious(self):
+    return f"My bank detiels are {self.Id},{self.Nationality} and {self.age}"
